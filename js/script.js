0de689b3bc66a93437b245abb1c29ac82343aa1e
markmccabe1 <markomccabe@hotmail.co.uk>
2019-11-20 23:15:32 +0000

    First draft of the website

---
+++ js/script.js
@@ -0,0 +1,47 @@
+$( document ).ready(function() {
+calculateWeddingCountdown();
+  });
+
+
+
+function calculateWeddingCountdown(){
+    
+    //Get today's date.
+    var now = new Date();
+ 
+ 
+    var christmasDay =  new Date("2020-09-11T13:30:00");
+
+ 
+    //Get the difference in seconds between the two days.
+    var diffSeconds = Math.floor((christmasDay.getTime() - now.getTime()) / 1000);
+ 
+    var days = 0;
+    var hours = 0;
+    var minutes = 0;
+    var seconds = 0;
+ 
+
+        //Convert these seconds into days, hours, minutes, seconds.
+        days = Math.floor(diffSeconds / (3600*24));
+        diffSeconds  -= days * 3600 * 24;
+        hours   = Math.floor(diffSeconds / 3600);
+        diffSeconds  -= hours * 3600;
+        minutes = Math.floor(diffSeconds / 60);
+        diffSeconds  -= minutes * 60;
+        seconds = diffSeconds;
+    
+
+    
+	  $("#day").text(days);
+	  $("#hour").html(hours);
+	  $("#min").html(minutes);
+	  $("#sec").html(seconds);
+	 
+    //Recursive call after 1 second using setTimeout
+    setTimeout(calculateWeddingCountdown, 1000);
+}
+
+
+
+
