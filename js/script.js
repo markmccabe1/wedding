caa607a06424b967a7ab7f961b81b8d1c4a7eacb
mark mccabe <markmccabe@MacBook-Pro.local>
2021-04-24 01:26:56 +0100

    Updating to use db

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 
 var $user = "";
+var $validationMessage = "";
 var loadCounter = 0;
+var db = null;
+var attendee = null;
+var attendees = [];
 
 $( document ).ready(function() {
 
@@ -8,7 +12,7 @@ $( document ).ready(function() {
 	calculateWeddingCountdown();
 	
 	$("#enterbtn").click(function(){
-	
+		$("#spinner").show();
 		onFormSubmit();
 	});
 	
@@ -19,7 +23,26 @@ $( document ).ready(function() {
 		showToast("Logged Out");
 	});
 
+	db = new restdb("60834b7328bf9b609975a5f9", null);
+	getUsers();
 });
+
+function getUsers(){
+	console.log("getUsers()");
+	var query = {}; // get all records
+	var hints = {"$max": 10, "$orderby": {"_id": -1}}; // top ten, sort by creation id in descending order
+	db.attendee.find(query, hints, function(err, res){
+	  if (!err){
+	    console.log(res);
+	    console.log("users retrieved");
+	    attendees = res;
+	  }else{
+	  	console.log("users not retrieved");
+	  }
+	});
+
+}
+
 
 function sanitizeString(str){
     str = str.replace(/[^a-z0-9‡Ž’—œ–Ÿ \.,_-]/gim,"");
@@ -39,56 +62,123 @@ function isInvalidString(str){
 
 function onFormSubmit(){
 	if(validateForm()){
-		createCookie("mmWeddingUser", $user);
-		showContentImmediately();
-		showToast("Welcome " + $user);
+		findUser();
 	}
 	else{
 		blankFields();
-		showToast("Incorrect Name/Code. Please try again");
+		showToast($validationMessage + ". Please try again");
 	}
 }
 
 
 
 function validateForm(){
+	$validationMessage = "";
 
-	$user = $("#nameInput").val();
+	var forename = $("#forenameInput").val();
+	var surname = $("#surnameInput").val();
+
+	$user = forename + " " + surname;
 	var code = $("#codeInput").val().toLowerCase();
 	
-	if(isInvalidString($user)){
+	if(isInvalidString(forename)){
+		$validationMessage = "Invalid Forename";
+		return false;
+	}
+
+	if(isInvalidString(surname)){
+		$validationMessage = "Invalid Surname";
 		return false;
 	}
 	
 	if(isInvalidString(code)){
+		$validationMessage = "Invalid Code";
 		return false;
 	}
 	
 	if($user == null || $user === ""){
+		$validationMessage = "Invalid Name";
 		return false;
 	}
-	
+
 	if(code == "2bmccabe"){
 		return true;
 	}
-	
+
+	$validationMessage = "Invalid Code";
 	return false;
 }
+
+function findUser(){
+	
+	var nameArray = $user.split(" ");
+	var forename = nameArray[0];
+	var surname = nameArray[1];
+
+
+	console.log("getUser" + forename + surname);
+	var result = false;
+	var query = {"forname" : forename, "surname": surname}; // get all records
+	var hints = {"$max": 10, "$orderby": {"_id": -1}}; // top ten, sort by creation id in descending order
+	db.attendee.find(query, hints, function(err, res){
+	  if (!err){
+	  	attendee = res[0];
+
+	  	if(attendee != null){
+	  		createCookie("mmWeddingUser", $user);
+			showContentImmediately();
+
+	  		if(attendee['attending'] == ""){
+	  			showBodyToast("You're Invited. Please RSVP below");
+		  	}
+		  	if (attendee['attending'] == "Yes"){
+		  		showBodyToast("Thanks for RSVP'ing.");
+		  	}
+	  	}
+	  	else{
+	  		showBodyToast("Can't find "+ $user +".");
+	  		blankFields();
+	  	}
+
+	  }
+	  else{
+	  	console.log(err);
+	  }
+	});
+}
+
+
 
 function blankFields(){
 	$user = "";
 	$("#loginForm").trigger("reset");
+	$("#spinner").hide();
+    $validationMessage = "";
+	loadCounter = 0;
+	attendee = null;
 	
 	
 }
 
 function showToast(message){
 	
-	var html = "<div><br><p>&nbsp;&nbsp;"+message+"&nbsp;&nbsp;</p></div>";
+	//var html = "<div><br><p>&nbsp;&nbsp;"+message+"&nbsp;&nbsp;</p></div>";
+	var html = "<div><center><p>&nbsp;&nbsp;"+message+"&nbsp;&nbsp;</p></center></div>";
+
 	
 	
 	$("#myToast").html(html);
 	$("#myToast").toast('show');
+}
+
+function showBodyToast(message){
+
+		var html = "<div><center><p>&nbsp;&nbsp;"+message+"&nbsp;&nbsp;</p></center></div>";
+	
+	
+	$("#myBodyToast").html(html);
+	$("#myBodyToast").toast('show');
+
 }
 
 
@@ -100,6 +190,7 @@ function validateUser(){
 
 	if(cookie != null){
 		$user = cookie;
+		findUser();
 		showContentImmediately();
 	}
 	else{
@@ -107,7 +198,7 @@ function validateUser(){
 			showContentImmediately();
 		}
 		else{
-					showLogin();
+			showLogin();
 		}
 	}
 }
@@ -168,6 +259,8 @@ function showContentImmediately(){
 }
 
 function showLogin(){
+		
+		$("#spinner").hide();
 		$("#access").show();
 		$("#content").hide();
 }
@@ -200,7 +293,18 @@ function removeCookie(name){
 }
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
